29340f1fbaebb9978e19d165992f05b40dc73cce
Liu Tang <66201467+LT60@users.noreply.github.com>
2020-11-04 23:47:02 +0800

    add the main menu

--- GraffitiGdiPluaaaas/GraffitiGdiPluaaaas/Resource.h
+++ GraffitiGdiPluaaaas/GraffitiGdiPluaaaas/Resource.h
@@ -1,21 +1,34 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ 生成的包含文件。
-// 由 GraffitiGdiPluaaaas.rc 使用
+// 供 GraffitiGdiPluaaaas.rc 使用
 //
-#define IDR_MAINFRAME					128
-#define IDM_ABOUTBOX					0x0010
-#define IDD_ABOUTBOX					100
-#define IDS_ABOUTBOX					101
-#define IDD_GRAFFITIGDIPLUAAAAS_DIALOG				102
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_GRAFFITIGDIPLUAAAAS_DIALOG  102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+#define ID_32781                        32781
+#define ID_32782                        32782
+#define ID_32774_                       32783
 
-// 新对象的下一组默认值
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32784
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
